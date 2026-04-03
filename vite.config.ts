8ac6e7fee9fa9893a933b68bd08440429a2b899b
ozan-fn <me@ozan.my.id>
2026-04-03 10:55:50 +0700

    Add Lucide preprocessor to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import lucidePreprocess from 'vite-plugin-lucide-preprocess';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({ plugins: [lucidePreprocess(), tailwindcss(), sveltekit()] });
